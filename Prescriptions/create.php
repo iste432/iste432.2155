55ba8ef80a7c921b5715533a9b09c07bad7edc3a
Yumi Kim <yumikimsk@gmail.com>
2016-04-08 13:32:58 -0400

    layout page

--- Prescriptions/create.php
+++ Prescriptions/create.php
@@ -1,1 +1,69 @@
-create prescription
+<?php include "../build.php"; ?>
+<html>
+<head>
+	<title>MyHealth</title>
+	<link href="../assets/css/font-awesome/font-awesome.min.css" rel="stylesheet">
+	<link href="../assets/css/font-awesome/font-awesome.css" rel="stylesheet">
+	<link href="../assets/css/prescriptions.css" rel="stylesheet">
+	<link href="../assets/css/styleNavbar.css" rel="stylesheet" />
+	<?php buildHead(); ?>
+</head>
+<body>
+	<?php buildBanner(); ?>
+
+	<div id="wrapper">
+
+		<?php buildNavigation(); ?>
+
+		<div id="content">
+			<h3 class="fill-header">Create Prescription</h3>
+			<form id="create-prescription-form">
+				<table id="create-prescription-table">
+					<!-- Physician ID should be pulled from session -->
+					<tr>
+						<td><label for="patient-id">Patient</label></td>
+						<td>
+							<select id="patient-id" name="patient-id">
+								<option>Test</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td><label for="medication-id">Medication</label></td>
+						<td>
+							<select id="medication-id" name="medication-id">
+								<option>Test</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td><label for="dosage">Dosage</label></td>
+						<td><input id="dosage" name="dosage" type="text"></td>
+					</tr>
+					<tr>
+						<td><label for="refills">Number of Refills</label></td>
+						<td><input id="refills" name="refills" type="text"></td>
+					</tr>
+					<tr>
+						<td><label for="exp-date">Expiration Date</label></td>
+						<td><input id="exp-date" name="exp-date" type="text"></td>
+					</tr>
+					<tr>
+						<td><label for="frequency">Frequency</label></td>
+						<td><input id="frequency" name="frequency" type="text"></td>
+					</tr>
+					<tr>
+						<td><label for="route">Route</label></td>
+						<td><input id="route" name="route" type="text"></td>
+					</tr>
+					<tr>
+						<td colspan="2"><button name="submit" value="create-prescription">Submit</button></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</div>
+
+	<?php buildFooter(); ?>
+</body>
+</html>
